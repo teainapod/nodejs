d4602cccd04cdaa647bef131d559ab28d82c2d1a
Sachin Gusain <sachingusain@gmail.com>
2024-12-07 15:41:07 -0500

    Part 3-Added Morgan logging
    Adding logging in the exercise.

--- part3/pbbackend/index.js
+++ part3/pbbackend/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+var morgan = require('morgan')
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body); 
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let phoneBook = [
   { 
     "id": "1",
